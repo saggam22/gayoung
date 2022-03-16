035c62b170f8348bf02f95f063d1285d7a2ec25f
집 <@192.168.219.100>
2022-03-16 23:26:30 +0900

    9th commit'

--- HelloWorld/src/com/prod/UserExample.java
+++ HelloWorld/src/com/prod/UserExample.java
@@ -21,16 +21,11 @@ public class UserExample {
 		u3.setUserPhone("010-5555-6666");
 		u3.setUserPoint(3000);
 		
-		System.out.println(u1.getUserInfo());
-		System.out.println(u2.getUserInfo());
-		System.out.println(u3.getUserInfo());
-		
-		
 		User[] users = {u1, u2, u3};
 		
 		for (int i=0; i<users.length; i++) {
 			if(users[i].getUserGrade() == 'A' && users[i].getUserPoint() >= 1000 ) {
-				System.out.println("A등급 이상이고 포인트 1000점 이상인 회원의 아이디는 " + users[i].getUserId());
+				System.out.println(users[i].getUserInfo());
 			}
 		}
 	}
